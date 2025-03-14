c32f569525311f9a6f6746770ddf334612d52cc8
James C. Burchill <69047523+jamesburchill@users.noreply.github.com>
2025-03-14 17:40:35 -0400

    ...

---
+++ email_template.py
@@ -0,0 +1,21 @@
+#  Copyright (c) 2025 - JamesBurchill.com - See LICENSE for details, otherwise All Rights Reserved
+#  File Location: /Users/jamesburchill/Documents/DEV/Gmail_Automator/email_template.py
+#  Last Updated: 3/14/25, 5:40 PM
+
+EMAIL_SUBJECT = "Reconnecting & Something New for You (Maybe?)"
+
+EMAIL_BODY = """
+Hey there!,
+
+It’s been a while! Back in the day ...
+
+Would love to hear what you’ve been up to as well—just hit reply and let me know!
+
+#StayFrosty!
+
+James C. Burchill
+https://jamesburchill.com
+
+P.S. If you remember me from my networking events, Microlearning Mastery is kind of like that—just delivered straight to your inbox with insights you can use every day. Hope to see you inside!Thank you.
+
+"""
